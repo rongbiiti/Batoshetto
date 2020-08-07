a53e6cd8c0665e4755a924432129b924cb5b36e7
ryugo_1220 <11807130@stu.kbc.ac.jp>
2020-08-07 11:04:31 +0900

    エフェクト絵素材追加

--- Effect.cpp
+++ Effect.cpp
@@ -9,7 +9,7 @@ Effect::Effect() {
 void Effect::DrawEffect(int x, int y, float angle) {
 	
 	if(effectcount<=12){
-	DrawRotaGraph(x + cosf(angle * DX_PI_F / 180.0f) * 45, y + sinf(angle * DX_PI_F / 180.0f) * 45, 1.5f, angle * DX_PI_F / 180.0f, i_ShotEffect[effectcount/3], TRUE);
+	DrawRotaGraph(x + cosf(angle * DX_PI_F / 180.0f) * 65, y + sinf(angle * DX_PI_F / 180.0f) * 65, 1.5f, angle * DX_PI_F / 180.0f, i_ShotEffect[effectcount/3], TRUE);
 	effectcount++;
 	}else{
 		EffectStatus = 0;		// エフェクトステータスを０に戻す
@@ -18,10 +18,10 @@ void Effect::DrawEffect(int x, int y, float angle) {
 
 // 画像読み込み
 void Effect::LoadImages() {
-	if (!(i_ShotEffect[0] = LoadGraph("Image/Gauge.png"))) return;
-	if (!(i_ShotEffect[1] = LoadGraph("Image/Gauge2.png"))) return;
-	if (!(i_ShotEffect[2] = LoadGraph("Image/Gauge.png"))) return;
-	if (!(i_ShotEffect[3] = LoadGraph("Image/Gauge2.png"))) return;
+	if (!(i_ShotEffect[0] = LoadGraph("Image/ShotEffect01.png"))) return;
+	if (!(i_ShotEffect[1] = LoadGraph("Image/ShotEffect02.png"))) return;
+	if (!(i_ShotEffect[2] = LoadGraph("Image/ShotEffect03.png"))) return;
+	if (!(i_ShotEffect[3] = LoadGraph("Image/ShotEffect04.png"))) return;
 
 }
 
